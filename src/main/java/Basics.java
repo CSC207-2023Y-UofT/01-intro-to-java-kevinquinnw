5e9dac20464e6b774bfd70113740af990ef633c1
Your Name <you@example.com>
2023-05-29 17:49:27 -0400

    Fulfill TODOs

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -44,6 +44,8 @@ public class Basics {
          *                Hello World!
          */
 
+        System.out.println("Hello World!");
+
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -63,7 +65,7 @@ public class Basics {
          *                value 100.
          */
 
-
+        int my_variable = 100;
 
         /* Do not remove the line below: if you did task 2 correctly, then
          * you should see the message "my_variable's value is 100" if you run
@@ -100,6 +102,9 @@ public class Basics {
          * Current count: 0
          */
 
+         for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
     }
 
@@ -143,6 +148,12 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        // Split word by space
+        String[] wordList = to_split.split(" ");
+        // For each word in our list, get the first character and add it to the string builder
+        for (String word : wordList) {
+            ret.append(word.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,6 +181,12 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+
+        for (int i=0; i < arr.length; i++) {
+            if (i % 2 != 0) {
+                current_sum = current_sum + arr[i];
+            }
+         }
 
         return current_sum;
     }
